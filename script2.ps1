2e2914e678eae046abb9833d0e909f8b79bd4cbd
Cals <88802566+victorcals@users.noreply.github.com>
2024-04-28 16:03:25 -0300

    Intro ao power

---
+++ script2.ps1
@@ -0,0 +1,11 @@
+﻿[int]$num1 = $args[0]
+[int]$num2 = $args[1]
+
+if($args.Length -lt 2){
+    echo " esse script fara uma multiplicação"
+    echo "script.ps1 numero1 e numero 2"
+}
+
+else {
+    echo($num1*$num2)
+}
---
+++ script3.ps1
@@ -0,0 +1,13 @@
+﻿$contador = 0
+ while($contador -lt 10){
+    $contador = $contador +1
+    echo "$contador"
+ }
+
+ for($contador =0; $contador -lt 15; $contador++){
+    echo $contador
+ }
+
+ foreach($numero in 1..100){
+    echo $numero
+ }
